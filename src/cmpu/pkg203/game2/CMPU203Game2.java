96c2370a479e28c0a866313090fd900d3fae42d6
Michael Goldman <migoldman@vassar.edu>
2014-11-08 18:11:04 -0500

    Hello game
    Hello, old friend

---
+++ src/cmpu/pkg203/game2/CMPU203Game2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmpu.pkg203.game2;
+
+/**
+ *
+ * @author michaelgoldman
+ */
+public class CMPU203Game2 {
+    //this is a scary thing
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
